cd9f83056fb51fffe4583391aa100ec60d58e03f
xVyrusz <cisnnerosbcrra@hotmail.com>
2020-01-18 11:15:36 -0600

    borrando el consolelog

--- js/comportamiento.js
+++ js/comportamiento.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     $("#mostrar").click(function(){
         $(".mundo").toggle();
-        console.log("Antes del if:",$("#mostrar").css("display"))
         if($(".mundo").css("display")=="none"){
             $("#mostrar").html("Mostrar Destinos");
         }else{
